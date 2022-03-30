fb5dea55a5484ec5cb8e65392174d12d1ccfc616
ManjuBanu <manjubanu0823@gmail.com>
2022-03-31 01:25:28 +0530

    Updates

---
+++ precache-manifest.67c9c446950a9d355a8b32e747f25d97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45eb2f65facc39628c2d237e1f5a7d1a",
+    "url": "/BurgerReactApp/index.html"
+  },
+  {
+    "revision": "de4b842af2833c22e369",
+    "url": "/BurgerReactApp/static/css/main.b05007ab.chunk.css"
+  },
+  {
+    "revision": "39316cf2dfdb120bd36b",
+    "url": "/BurgerReactApp/static/js/2.87bcb8f7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/BurgerReactApp/static/js/2.87bcb8f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de4b842af2833c22e369",
+    "url": "/BurgerReactApp/static/js/main.0820869a.chunk.js"
+  },
+  {
+    "revision": "eae9e132d2a0733e5b3c",
+    "url": "/BurgerReactApp/static/js/runtime-main.3af61049.js"
+  }
+]);
